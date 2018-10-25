45c20621ec08f05e2294f125fb5f6642fd2dedb8
gaoyanguo <gaoyanguo@ultrapower.com.cn>
2018-10-25 09:21:25 +0800

    登录限制提交

--- src/main/java/com/monkeyzi/oauth/security/UserDetailServiceImpl.java
+++ src/main/java/com/monkeyzi/oauth/security/UserDetailServiceImpl.java
@@ -1,21 +1,21 @@
 package com.monkeyzi.oauth.security;
 
-import com.google.common.collect.Lists;
+import com.monkeyzi.oauth.common.GlobalConstant;
 import com.monkeyzi.oauth.entity.domain.User;
+import com.monkeyzi.oauth.exception.LoginFailLimitException;
 import com.monkeyzi.oauth.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -26,7 +26,6 @@ public class UserDetailServiceImpl  implements UserDetailsService {
     @Resource
     private RedisTemplate<String,String> redisTemplate;
 
-    private static final List<User> users=Lists.newArrayList();
 
 
 
@@ -34,6 +33,12 @@ public class UserDetailServiceImpl  implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        String failKey=GlobalConstant.Sys.SYS_LOGIN_FAIL_LIMIT+":"+s;
+        String value=redisTemplate.opsForValue().get(failKey);
+        Long timeRest = redisTemplate.getExpire(failKey, TimeUnit.MINUTES);
+        if (StringUtils.isNotBlank(value)){
+            throw new LoginFailLimitException("登录错误次数超过限制，请"+timeRest+"分钟后再试");
+        }
         Optional<User> user = Optional.ofNullable(userService.findUserByUserName(s));
         if (!user.isPresent()){
             throw new UsernameNotFoundException("用户名不存在");
