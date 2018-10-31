e1b2fc4bf4b36b1ebc138787fe6813bcfff1203f
gaoyanguo <gaoyanguo@ultrapower.com.cn>
2018-10-31 15:21:48 +0800

    提交

--- src/main/java/com/monkeyzi/oauth/security/code/CustomAuthenticationProvider.java
+++ src/main/java/com/monkeyzi/oauth/security/code/CustomAuthenticationProvider.java
@@ -3,7 +3,6 @@ package com.monkeyzi.oauth.security.code;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
